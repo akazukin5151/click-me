561194b6f8be3f955aacf7c87e943b01269493bf
akazukin5151 <tsuiyikching@protonmail.com>
2022-07-13 23:57:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# click-me
+
+This is a handy tool that runs a cross platform simple http server on the program's current directory, and launches `http://0.0.0.0:8000/index.html` in the user's default browser
+
+The http server binary is from https://github.com/TheWaWaR/simple-http-server and the precompiled binaries are vendored in `http-server-vendor`
+
+The binaries are currently version 0.6.1
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::path::Path;
+use std::process::ExitStatus;
+use std::process::Command;
+use std::fs;
+use std::thread;
+
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+
+#[cfg(target_os = "linux")]
+fn include_bin() -> &'static [u8] {
+    include_bytes!("../http-server-vendor/http-server-linux")
+}
+
+#[cfg(target_os = "macos")]
+fn include_bin() -> &'static [u8] {
+    todo!()
+}
+
+#[cfg(target_os = "windows")]
+fn include_bin() -> &'static [u8] {
+    include_bytes!("../http-server-vendor/http-server-windows.exe")
+}
+
+#[cfg(unix)]
+fn set_executable(path: &'static str) {
+    let file = fs::File::open(path).unwrap();
+    let mut p = file.metadata().unwrap().permissions();
+    p.set_mode(755);
+    fs::set_permissions(path, p).unwrap();
+}
+
+#[cfg(target_os = "windows")]
+fn set_executable(path: &'static str) {
+}
+
+fn execute_bin(path: &'static str) -> Result<ExitStatus, &str> {
+    Command::new(path).arg("--cors").status().map_err(|_| "Failed to launch command")
+}
+
+#[cfg(unix)]
+const EXE_PATH: &'static str = "/tmp/http-server";
+
+#[cfg(target_os = "windows")]
+const EXE_PATH: &'static str = "/tmp/http-server.exe";
+
+fn main() {
+    if Path::new(EXE_PATH).exists() {
+        fs::remove_file(EXE_PATH).unwrap();
+    };
+    let exe = include_bin();
+    fs::write(EXE_PATH, exe).unwrap();
+    set_executable(EXE_PATH);
+    thread::spawn(move || {
+        execute_bin(EXE_PATH).unwrap();
+    });
+    open::that("http://0.0.0.0:8000/index.html").unwrap();
+}
